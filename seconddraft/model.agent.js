9e046ecff79d9d9aec5fafb250fb321ccdffb750
Mjumbe Poe <mjumbewu@gmail.com>
2020-05-08 00:20:58 -0400

    Convert Agent and World to class syntax (like Boundary).

--- seconddraft/model.agent.js
+++ seconddraft/model.agent.js
@@ -1,58 +1,60 @@
 import { Vector } from './geometry.js'
 
-function Agent(params) {
-  this.x = params.x
-  this.y = params.y
-  this.radius = params.radius
-  this.velocity = params.velocity || Vector.fromPolar(params.speed, params.direction)
-  this.time = params.time || 0
-}
-
-Agent.prototype.step = function(Δt=1) {
-  // Calculate the change in position based on how much time has ellapsed (dt)
-  const Δx = this.velocity.x * Δt
-  const Δy = this.velocity.y * Δt
-
-  // Calculate new values for x and y
-  const x = this.x + Δx
-  const y = this.y + Δy
-
-  // Calculate the new time
-  const time = this.time + Δt
-
-  return new Agent({
-    ...this,
-    x, y,
-    time,
-  })
-}
-
-Agent.prototype.bounce = function(bounceVector) {
-  if (bounceVector.x === 0 && bounceVector.y === 0) {
-    return this
+class Agent {
+  constructor(params) {
+    this.x = params.x
+    this.y = params.y
+    this.radius = params.radius
+    this.velocity = params.velocity || Vector.fromPolar(params.speed, params.direction)
+    this.time = params.time || 0
   }
 
-  // vf = v - 2 (v • n) n
-  //
-  // v is the current directional vector
-  // vf (or xf - x0) is the after-bounce directional vector
+  step(Δt=1) {
+    // Calculate the change in position based on how much time has ellapsed (dt)
+    const Δx = this.velocity.x * Δt
+    const Δy = this.velocity.y * Δt
 
-  // We want the normal vector to have a magnitude of 1, so let's "normalize" it.
-  const n = bounceVector.unit
-  const v = this.velocity
+    // Calculate new values for x and y
+    const x = this.x + Δx
+    const y = this.y + Δy
 
-  // Get the dot product of v and n. If the dot product is positive, then the
-  // agent is travelling in the same direction as the normal vector and we should
-  // not bounce.
-  const dp = n.dot(v)
-  if (dp >= 0) { return this }
+    // Calculate the new time
+    const time = this.time + Δt
 
-  const vf = v.plus(n.times(-2 * dp))
+    return new Agent({
+      ...this,
+      x, y,
+      time,
+    })
+  }
 
-  return new Agent({
-    ...this,
-    velocity: vf
-  })
+  bounce(bounceVector) {
+    if (bounceVector.x === 0 && bounceVector.y === 0) {
+      return this
+    }
+
+    // vf = v - 2 (v • n) n
+    //
+    // v is the current directional vector
+    // vf (or xf - x0) is the after-bounce directional vector
+
+    // We want the normal vector to have a magnitude of 1, so let's "normalize" it.
+    const n = bounceVector.unit
+    const v = this.velocity
+
+    // Get the dot product of v and n. If the dot product is positive, then the
+    // agent is travelling in the same direction as the normal vector and we should
+    // not bounce.
+    const dp = n.dot(v)
+    if (dp >= 0) { return this }
+
+    const vf = v.plus(n.times(-2 * dp))
+
+    return new Agent({
+      ...this,
+      velocity: vf
+    })
+  }
 }
 
 export { Agent }
--- seconddraft/model.world.js
+++ seconddraft/model.world.js
@@ -1,60 +1,62 @@
 import { Segment, Vector } from './geometry.js'
 import { zipLongest } from './utils.js'
 
-function World(params) {
-  this.agents = params.agents || []
-  this.boundaries = params.boundaries || []
-  this.time = params.time || 0
-}
+class World {
+  constructor(params) {
+    this.agents = params.agents || []
+    this.boundaries = params.boundaries || []
+    this.time = params.time || 0
+  }
 
-World.prototype.step = function(Δt=1) {
-  let getBounceVector = (agentStates) => {
-    const [old_, new_] = agentStates
-    const Δx = new_.x - old_.x
-    const Δy = new_.y - old_.y
-    const zeroVector = new Vector(0, 0)
+  step(Δt=1) {
+    let getBounceVector = (agentStates) => {
+      const [old_, new_] = agentStates
+      const Δx = new_.x - old_.x
+      const Δy = new_.y - old_.y
+      const zeroVector = new Vector(0, 0)
 
-    let agentBounceVector = zeroVector
-    for (const boundary of this.boundaries) {
-      // First we figure out which side of the boundary the original agent
-      // center was on. There is a good explanation of why the following does
-      // that at https://math.stackexchange.com/a/274728/783341.
-      const side = ((old_.x - boundary.x1) * (boundary.y2 - boundary.y1) -
-                    (old_.y - boundary.y1) * (boundary.x2 - boundary.x1)) >= 0 ? 1 : -1
+      let agentBounceVector = zeroVector
+      for (const boundary of this.boundaries) {
+        // First we figure out which side of the boundary the original agent
+        // center was on. There is a good explanation of why the following does
+        // that at https://math.stackexchange.com/a/274728/783341.
+        const side = ((old_.x - boundary.x1) * (boundary.y2 - boundary.y1) -
+                      (old_.y - boundary.y1) * (boundary.x2 - boundary.x1)) >= 0 ? 1 : -1
 
-      // Then we find the nearest point on the edge of the agent to the
-      // boundary. This is going to be the point along the agents edge in the
-      // direction (or opposite direction, depending on the side) of the
-      // boundary's normal vector.
-      const normal = boundary.normal
-      const oldNearestX = old_.x - (normal.x * old_.radius) * side
-      const oldNearestY = old_.y - (normal.y * old_.radius) * side
-      const newNearestX = new_.x - (normal.x * new_.radius) * side
-      const newNearestY = new_.y - (normal.y * new_.radius) * side
-      const nearestPointPath = new Segment(oldNearestX, oldNearestY, newNearestX, newNearestY)
+        // Then we find the nearest point on the edge of the agent to the
+        // boundary. This is going to be the point along the agents edge in the
+        // direction (or opposite direction, depending on the side) of the
+        // boundary's normal vector.
+        const normal = boundary.normal
+        const oldNearestX = old_.x - (normal.x * old_.radius) * side
+        const oldNearestY = old_.y - (normal.y * old_.radius) * side
+        const newNearestX = new_.x - (normal.x * new_.radius) * side
+        const newNearestY = new_.y - (normal.y * new_.radius) * side
+        const nearestPointPath = new Segment(oldNearestX, oldNearestY, newNearestX, newNearestY)
 
-      if (boundary.isCrossedBy(nearestPointPath)) {
-        agentBounceVector = agentBounceVector.plus(normal.times(side))
+        if (boundary.isCrossedBy(nearestPointPath)) {
+          agentBounceVector = agentBounceVector.plus(normal.times(side))
+        }
       }
+      return agentBounceVector
     }
-    return agentBounceVector
+
+    let getBouncedAgent = (args) => {
+      const [agent, vector] = args
+      return agent.bounce(vector)
+    }
+
+    const steppedAgents = this.agents.map(a => a.step(Δt))
+    const bounceVectors = Array.from(zipLongest(this.agents, steppedAgents)).map(getBounceVector)
+    const agents = Array.from(zipLongest(steppedAgents, bounceVectors)).map(getBouncedAgent)
+    const time = this.time + Δt
+
+    return new World({
+      ...this,
+      agents,
+      time,
+    })
   }
-
-  let getBouncedAgent = (args) => {
-    const [agent, vector] = args
-    return agent.bounce(vector)
-  }
-
-  const steppedAgents = this.agents.map(a => a.step(Δt))
-  const bounceVectors = Array.from(zipLongest(this.agents, steppedAgents)).map(getBounceVector)
-  const agents = Array.from(zipLongest(steppedAgents, bounceVectors)).map(getBouncedAgent)
-  const time = this.time + Δt
-
-  return new World({
-    ...this,
-    agents,
-    time,
-  })
 }
 
 export { World }
