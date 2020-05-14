68299f53ffd69f56dfdd53f1521b2191a6437509
Mjumbe Poe <mjumbewu@gmail.com>
2020-05-14 00:55:01 -0400

    Inequality cleanup

--- seconddraft/utils.js
+++ seconddraft/utils.js
@@ -7,7 +7,7 @@ function* zipLongest(array1, array2) {
 
 // Floating point math is, literally, the devil. So, let's use a threshold
 // of 5 decimal places for all of our numbers, for the sake of our sanity.
-function eq(a, b, t=0.000001) { return (a <= b + t && a >= b - t) }
+function eq(a, b, t=0.000001) { return (b - t <= a && a <= b + t) }
 function lte(a, b, t=0.000001) { return (a <= b + t) }
 function gte(a, b, t=0.000001) { return (a >= b - t) }
 
